69d0b4047b590d514d6bffa1007fe79ada94ae39
Julia Zdzilowska <135221630+jzdzilowska@users.noreply.github.com>
2024-09-26 01:21:59 +0200

    feat:basic functionality implemented

--- regiojet_scraper.py
+++ regiojet_scraper.py
@@ -2,10 +2,12 @@ import time
 from datetime import datetime
 import pandas as pd
 import numpy as np
-from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import requests
+import lxml
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -100,6 +102,7 @@ def insert_ticket(df, ticket_id, origin, departure_date, departure_hour, destina
         'fare_type': fare_type,
         'available': available,
     }
+    print(f"Inserting ticket: {new_ticket}")  # Debug print
     # Append the new ticket to the DataFrame
     df.loc[len(df)] = new_ticket
     return df
@@ -107,74 +110,74 @@ def insert_ticket(df, ticket_id, origin, departure_date, departure_hour, destina
 # Scrapes the ticket information from the website
 def scrape_tickets(df, origin, destination, departure_date, fare_type):
     url = parse_url(origin, destination, departure_date, fare_type)
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        soup=BeautifulSoup(driver.page_source, 'lxml')
+    service = Service(executable_path="/Users/zdzilowska/Desktop/comp/mine/RegiojetScraper/chromedriver")
+    driver = webdriver.Chrome(service=service)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
         
-        # find all dates
-        date_headers = soup.find_all('p', class_='mt-0.5 lg:mt-1 mb-2 lg:mb-3 sm:text-base font-bold')
+    # find all dates
+    date_headers = soup.find_all('p', class_='mt-0.5 lg:mt-1 mb-2 lg:mb-3 sm:text-base font-bold')
+    # Loop through each date and scrape associated ticket information
+    for date_header in date_headers:
+        date = date_header.text.strip()  # Extract the date text
+        
+        # Convert date to YYYY-MM-DD format
+        # Unconverted: ex. "Wednesday, 25. September 2024"
+        try:
+            date_obj = datetime.strptime(date, "%A, %d. %B %Y")
+            formatted_date = date_obj.strftime("%Y-%m-%d")
+        except ValueError as e:
+            print(f"Date format error: {e}")
+            continue
 
-        # Loop through each date and scrape associated ticket information
-        for date_header in date_headers:
-            date = date_header.text.strip()  # Extract the date text
-            
-            # Convert date to YYYY-MM-DD format
-            # Unconverted: ex. "Wednesday, 25. September 2024"
-            try:
-                date_obj = datetime.strptime(date, "%A, %d. %B %Y")
-                formatted_date = date_obj.strftime("%Y-%m-%d")
-            except ValueError as e:
-                print(f"Date format error: {e}")
-                continue
+        # Find the next sibling; should be the 'w-full flex flex-col' containing the tickets for the date above
+        ticket_container = date_header.find_next('div', class_='w-full flex flex-col')
 
-            # Find the next sibling; should be the 'w-full flex flex-col' containing the tickets for the date above
-            ticket_container = date_header.find_next('div', class_='w-full flex flex-col')
+        if ticket_container:
+            tickets = ticket_container.find_all('li')  # Each ticket is in an <li> tag
+            # Loop through tickets & extract details
+            for ticket in tickets:
+                # ! time
+                time_info = ticket.find('h2', class_='h3')  # Extract departure and arrival time
+                if time_info:
+                    departure_hour, arrival_hour = time_info.text.split(' - ')
 
-            if ticket_container:
-                tickets = ticket_container.find_all('li')  # Each ticket is in an <li> tag
-                # Loop through tickets & extract details
-                for ticket in tickets:
-                    # ! time
-                    time_info = ticket.find('h2', class_='h3')  # Extract departure and arrival time
-                    if time_info:
-                        departure_hour, arrival_hour = time_info.text.split(' - ')
-
-                    # ! price
-                    price_info = ticket.find('button', class_='inline-flex items-center justify-center px-2.5 rounded-sm font-bold')
-                    if price_info:
-                        # extract last element; normally button text = 'from €19.9', after split & [-1] = '€19.9'
-                        price = price_info.text.strip().split(' ')[-1] 
-                    
-                    # ! seats available 
-                    # Available seats (free seats)
-                    if seats_info:
-                        seats_text = seats_info.text.strip()  # Get the text
-                        if "sold out" in seats_text.lower():  # Check if 'sold out' is in the text
-                            available_seats = 0  # Set to 0 if sold out
-                        else:
-                            available_seats = seats_text.split()[-1]  # Extract the number from the text
+                # ! price
+                price_info = ticket.find('button', class_='inline-flex items-center justify-center px-2.5 rounded-sm font-bold transition focus:outline-none focus-visible:outline-none focus-visible:shadow-border hover:shadow-modal cursor-pointer bg-primary-blue text-white border-none hover:bg-secondary-bluedark focus-visible:bg-secondary-bluedark whitespace-nowrap h-5 whitespace-nowrap')
+                if price_info:
+                    # extract last element; normally button text = 'from €19.9', after split & [-1] = '€19.9'
+                    price = price_info.text.strip().split(' ')[-1] 
+                
+                # ! seats available 
+                # Available seats (free seats)
+                seats_info = ticket.find('span', class_='sr-only')
+                if seats_info:
+                    seats_text = seats_info.text.strip()  # Get the text
+                    if "sold out" in seats_text.lower():  # Check if 'sold out' is in the text
+                        available_seats = 0  # Set to 0 if sold out
                     else:
-                        available_seats = 0
+                        available_seats = seats_text.split()[-1]  # Extract the number from the text
+                else:
+                    available_seats = 0
 
-                    # ! booking info
-                    booking_button = ticket.find('button', {'data-id': lambda x: x and 'connection-card' in x})
-                    if booking_button:
-                        # Extract the data-id attribute value
-                        data_id = booking_button['data-id']  # 'connection-card-price-7070534785,7330426712'
-                        # Split the IDs if there are multiple ones separated by a comma
-                        ticket_ids = data_id.split('-')[-1].split(',')
-                        # Extract the first ticket ID 
-                        # TODO: handle multiple
-                        primary_ticket_id = ticket_ids[0]  # '7070534785'
+                # ! booking info
+                booking_button = ticket.find('button', {'data-id': lambda x: x and 'connection-card' in x})
+                if booking_button:
+                    # Extract the data-id attribute value
+                    data_id = booking_button['data-id']  # 'connection-card-price-7070534785,7330426712'
+                    # Split the IDs if there are multiple ones separated by a comma
+                    ticket_ids = data_id.split('-')[-1].split(',')
+                    # Extract the first ticket ID 
+                    # TODO: handle multiple
+                    primary_ticket_id = ticket_ids[0]  # '7070534785'
 
-                    print(f"Date: {date}")
-                    print(f"Departure: {departure_hour}, Arrival: {arrival_hour}")
-                    print(f"Price: {price}")
-                    print('-' * 40)
-                    insert_ticket(df, primary_ticket_id, origin, formatted_date, departure_hour, destination, formatted_date, arrival_hour, price, fare_type, available_seats)
-    else: 
-        print(f"Failed to fetch data from {url}")   
+                print(f"Date: {date}")
+                print(f"Departure: {departure_hour}, Arrival: {arrival_hour}")
+                print(f"Price: {price}")
+                print('-' * 40)
+                insert_ticket(df, primary_ticket_id, origin, formatted_date, departure_hour, destination, formatted_date, arrival_hour, price, fare_type, available_seats)  
+    driver.quit() 
     return "success"
 
 # Prompt user input from the command line
